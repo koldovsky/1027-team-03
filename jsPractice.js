60c662833646788a916ba4b1a873c1e64dd5c5ea
satedone <nz@intop.dp.ua>
2023-06-19 16:09:14 +0300

    add too solutions

--- jsPractice.js
+++ jsPractice.js
@@ -1,1 +1,15 @@
-Вставляємо найраціональніше рішення завдань з codewars
+// Insert the most rational solution to tasks from codewars here:
+
+// Task 1. Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+
+
+
+
+// Task 6. Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+  }
