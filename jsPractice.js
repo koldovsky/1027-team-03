7da1f8ca60a9a225e9f6aff4af45dae6956f4b4f
Ilkarion <122636254+Ilkarion@users.noreply.github.com>
2023-06-19 17:00:17 +0300

    Update jsPractice.js
    change task 2

--- jsPractice.js
+++ jsPractice.js
@@ -7,11 +7,7 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 // Task 2. Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
-    if (num > 0) {
-      return -num;
-    } else {
-      return num;
-    }
+    if(num<=0){return num;}else{return -num;}
   }
 
  // Task 3. Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
