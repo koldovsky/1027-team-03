11413ea20aeab0389ce062d257b1a4f8a39d10d6
Ilkarion <122636254+Ilkarion@users.noreply.github.com>
2023-06-21 16:33:49 +0300

    Update jsPractice02.js
    Added part "Поглиблені, додаткові задачки"

--- jsPractice02.js
+++ jsPractice02.js
@@ -14,7 +14,6 @@ var min = function(list) {
 }
 
 var max = function(list) {
-  
   return Math.max(...list)
 }
 
@@ -26,7 +25,28 @@ function min(arr, toReturn) {
   return toReturn === 'value' ? minValue : minIndex;
 }
 
-// Task 5. 
+-----Поглиблені, додаткові задачки-----
+  
+// Task 5. A wolf in sheep's clothing https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+function warnTheSheep(queue) {
+  const lastAnimal = queue.length;
+  const wolfWhere = queue.indexOf("wolf")+1;
+  if(lastAnimal == wolfWhere)return "Pls go away and stop eating my sheep";
+  return `Oi! Sheep number ${lastAnimal-wolfWhere}! You are about to be eaten by a wolf!`;
+}
 
-// Task 6. 
+// Task 6. Beginner - Lost Without a Map https://www.codewars.com/kata/57f781872e3d8ca2a000007e/solutions/javascript
+function maps(x) {
+  const doubleArr = x.map((item)=> item*2);
+  return doubleArr;
+}
 
+//Task 7 Find the first non-consecutive number https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/solutions
+function firstNonConsecutive (arr) {
+  let num = arr[0];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i]-num != 1){num = arr[i]; return num;}
+    num = arr[i];
+  }
+  return null;
+}
