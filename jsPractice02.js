30345ee2f3af309ff62b83a67c3caee307520a20
satedone <nz@intop.dp.ua>
2023-06-21 19:45:08 +0300

    corrected mistakes

--- jsPractice02.js
+++ jsPractice02.js
@@ -25,7 +25,7 @@ function min(arr, toReturn) {
   return toReturn === 'value' ? minValue : minIndex;
 }
 
------Поглиблені, додаткові задачки-----
+// -----Поглиблені, додаткові задачки-----
   
 // Task 5. A wolf in sheep's clothing https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
 function warnTheSheep(queue) {
