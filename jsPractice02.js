4aca393f4ea5c787dcc13bfe6c6b5059de928596
satedone <nz@intop.dp.ua>
2023-06-21 18:10:24 +0300

    corrected

--- jsPractice02.js
+++ jsPractice02.js
@@ -9,18 +9,18 @@ function DNAtoRNA(dna) {
 }
 
 // Task 3. Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-var min = function(list) {
+const min = function(list) {
   return Math.min(...list)
 }
 
-var max = function(list) {
+const max = function(list) {
   return Math.max(...list)
 }
 
 // Task 4. Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 function min(arr, toReturn) {
-  var minValue = Math.min.apply(null, arr);
-  var minIndex = arr.indexOf(minValue);
+  let minValue = Math.min.apply(null, arr);
+  let minIndex = arr.indexOf(minValue);
 
   return toReturn === 'value' ? minValue : minIndex;
 }
