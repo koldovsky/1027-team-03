13696ac47df11496e7e2f69d7c23b0cf12ac95a2
satedone <nz@intop.dp.ua>
2023-06-23 12:32:24 +0300

    add practice03.js

---
+++ js/jsPractice03.js
@@ -0,0 +1,28 @@
+// TASK >>>>> Find the smallest integer in the array
+// LINK >>>>> https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
+  
+  // TASK >>>>> Geometry Basics: Circle Circumference in 2D
+  // LINK >>>>> https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+  
+  function circleCircumference(circle) {
+    return 2 * Math.PI * circle.radius;
+  }
+  
+  
+  // TASK >>>>> Training JS #12: loop statement --for..in and for..of
+  // LINK >>>>>  https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+  
+  function giveMeFive(obj) {
+    var five = [];
+    for (var key in obj) {
+      if (key.length === 5) five.push(key);
+      if (obj[key].length === 5) five.push(obj[key]);
+    }
+    return five;
+  }
--- js/sated.js
+++ js/sated.js
@@ -1,44 +1,68 @@
-// TASK >>>>> A wolf in sheep's clothing
-// LINK >>>>> https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+// TASK >>>>> Find the smallest integer in the array
+// LINK >>>>> https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 
-// Translate:
-// Вовків повернули в Великобританію. Ви є фермером овець і тепер вас турбують вовки,
-// які притворяються овцями. На щастя, ви добре впізнаєте їх.
-// Попереджайте овець, яка стоїть перед вовком, що її збираються з'їсти. Пам'ятайте, 
-// що ви стоїте на передньому кінці черги, яка знаходиться в кінці масиву:
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
 
-// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep] (ВИ ЗНАХОДИТЕСЬ ТУТ НА ПЕРЕДНЬОМУ КІНЦІ ЧЕРГИ)
-// 7 6 5 4 3 2 1
+// TASK >>>>> Geometry Basics: Circle Circumference in 2D
+// LINK >>>>> https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
-// Якщо вовк є найближчим тваринам до вас, поверніть "Pls go away and stop eating my sheep" 
-// (Будь ласка, піди геть і перестань їсти моїх овець"). В іншому випадку поверніть "Oi! Sheep number N! You are about to be eaten by a wolf!" (Ой! Овця номер N! Вас збираються з'їсти вовком!"), де N - позиція овці в черзі.
-
-
-// РІШЕННЯ:
-
-// 1. Отримати позицію вовка в черзі:
-
-// 2. Знайти індекс "wolf" в масиві queue за допомогою методу indexOf().
-// Зберегти отриманий індекс у змінну wolfIndex.
-// Перевірити, чи вовк є найближчим тваринам до вас:
-
-// 3. Порівняти wolfIndex з довжиною масиву queue мінус 1.
-// Якщо wolfIndex дорівнює довжині масиву queue мінус 1, то вовк є найближчим тваринам.
-// У такому випадку повернути рядок "Pls go away and stop eating my sheep".
-// Якщо вовк не є найближчим тваринам:
-
-// 4. Визначити позицію овці, яка знаходиться перед вовком у черзі.
-// Позиція овці буде рівна довжині масиву queue мінус wolfIndex мінус 1.
-// Повернути рядок "Oi! Sheep number N! You are about to be eaten by a wolf!", де N - позиція овці в черзі.
-
-function warnTheSheep(queue) {
-    const wolfIndex = queue.indexOf("wolf");                                            // Отримати позицію вовка в черзі
-    if (wolfIndex === queue.length - 1) {                                               // Перевірити, чи вовк є найближчим тваринам
-      return "Pls go away and stop eating my sheep";                                    // Вовк є найближчим тваринам
-    } else {
-      const sheepPosition = queue.length - wolfIndex - 1;                               // Визначити позицію овці перед вовком
-      return `Oi! Sheep number ${sheepPosition}! You are about to be eaten by a wolf!`; // Овця буде з'їдена вовком
-    }
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
 }
 
 
+// TASK >>>>> Training JS #12: loop statement --for..in and for..of
+// LINK >>>>>  https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj) {
+  var five = [];
+  for (var key in obj) {
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]);
+  }
+  return five;
+}
+
+
+
+
+
+
+
+// TASK >>>>> Training JS #11: loop statement --break, continue
+// LINK >>>>>  https://www.codewars.com/kata/5721c189cdd71194c1000b9b
+
+function grabDoll(dolls) {
+  var bag = [];
+  var count = 0;
+  for (var i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      bag.push(dolls[i]);
+      count++;
+      if (count === 3) {
+        break;
+      }
+    } else {
+      continue;
+    }
+  }
+  return bag;
+}
+
+
+// TASK >>>>> A wolf in sheep's clothing
+// LINK >>>>> https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+function warnTheSheep(queue) {
+  const wolfIndex = queue.indexOf("wolf");                                            
+  if (wolfIndex === queue.length - 1) {                                               
+    return "Pls go away and stop eating my sheep";                                    
+  } else {
+    const sheepPosition = queue.length - wolfIndex - 1;                               
+    return `Oi! Sheep number ${sheepPosition}! You are about to be eaten by a wolf!`; 
+  }
+}
