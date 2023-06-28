cc3e68868a8e166df89157c1aba5dc38dba7f8b9
Ilkarion <myfirststepsprogramming@gmail.com>
2023-06-28 16:58:36 +0300

    Upgrade 'our-frones' part

--- js/our-drones-carousel.js
+++ js/our-drones-carousel.js
@@ -1,3 +1,11 @@
+//-----------------Animation-characteristics---------------
+const characteristicsBtn = document.querySelector('.open-characteristics');
+const stickVer = document.querySelector('.stick-ver');
+
+characteristicsBtn.addEventListener('click', () =>{
+    stickVer.classList.toggle('opacity-stick');
+});
+//--------------------------END-----------------------------
 const slides = [
     ``,
     ``,
