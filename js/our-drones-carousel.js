9a3089df27bd447751c193cc959996a1c4de4c24
Ilkarion <myfirststepsprogramming@gmail.com>
2023-06-29 14:25:09 +0300

    Correcting my part 'our-drones'

--- js/our-drones-carousel.js
+++ js/our-drones-carousel.js
@@ -1,3 +1,238 @@
+
+const slidesInfo = [
+        `
+        <div class="margin-righ">
+            <img src="./img/our-drones/dji-mavik-zoom.png" alt="dji-mavik-zoom_Pic" class="drone-pic">
+            <span class="text-under-pic">
+                <span class="drone-name">DJI MAVIC 2 ZOOM</span>
+                <span class="drone-price-rent">from $120/mo.</span>
+            </span>
+            <img src="./img/our-drones/green-line-deco.svg" alt="">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                        <div class="open-characteristics">
+                            <div class="align-stick-hor">
+                                <span class="stick-hor">—</span>
+                                <div class="align-stick-ver"><span class="stick-ver">|</span></div>
+                            </div>
+                            <div class="characteristic">
+                                <span>Characteristics</span>
+                            </div>
+                        </div>
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                      <div class="accordion-body characteristics-text">
+                        <div class="margin-characteristics">
+                            <span class="weight">Weight:</span>
+                        </div>
+                        <span>905 g</span>
+                        <div class="margin-characteristics">
+                            <span class="speed">Max.speed:</span>
+                        </div>
+                        <span>72km/h(S-mode)</span>
+                        <div class="margin-characteristics">
+                            <span class="battery">Battery:</span>
+                        </div>
+                        <span>13830mAh</span>
+                        <div class="margin-characteristics">
+                            <span class="resolution">Max. resolution:</span>
+                        </div>
+                        <span>12MP</span>
+                        <div class="margin-characteristics">
+                            <span class="flight-time">Max, flight time:</span>
+                        </div>
+                        <span>31 minutes</span>
+                      </div>
+                </div>
+            </div>
+        </div>`,
+        
+    
+        `
+        <div class="margin-righ">
+            <img src="./img/our-drones/dji-mavik-pro.png" alt="dji-mavik-zoom_Pic" class="drone-pic">
+            <span class="text-under-pic">
+                <span class="drone-name">DJI MAVIC 2 PRO</span>
+                <span class="drone-price-rent">from $120/mo.</span>
+            </span>
+            <img src="./img/our-drones/green-line-deco.svg" alt="">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                        <div class="open-characteristics">
+                            <div class="align-stick-hor">
+                                <span class="stick-hor">—</span>
+                                <div class="align-stick-ver"><span class="stick-ver">|</span></div>
+                            </div>
+                            <div class="characteristic">
+                                <span>Characteristics</span>
+                            </div>
+                        </div>
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                      <div class="accordion-body characteristics-text">
+                        <div class="margin-characteristics">
+                            <span class="weight">Weight:</span>
+                        </div>
+                        <span>907 g</span>
+                        <div class="margin-characteristics">
+                            <span class="speed">Max.speed:</span>
+                        </div>
+                        <span>72km/h(S-mode)</span>
+                        <div class="margin-characteristics">
+                            <span class="battery">Battery:</span>
+                        </div>
+                        <span>3850mAh</span>
+                        <div class="margin-characteristics">
+                            <span class="resolution">Max. resolution:</span>
+                        </div>
+                        <span>20MP</span>
+                        <div class="margin-characteristics">
+                            <span class="flight-time">Max, flight time:</span>
+                        </div>
+                        <span>31 minutes</span>
+                      </div>
+                </div>
+            </div>
+        </div>`,
+
+
+        `
+        <div class="margin-righ">
+            <img src="img/our-drones/gopro-karma.png" alt="dji-mavik-zoom_Pic" class="drone-pic">
+            <span class="text-under-pic">
+                <span class="drone-name">GOPRO KARMA DRONE</span>
+                <span class="drone-price-rent">from $110/mo.</span>
+            </span>
+            <img src="./img/our-drones/green-line-deco.svg" alt="">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                        <div class="open-characteristics">
+                            <div class="align-stick-hor">
+                                <span class="stick-hor">—</span>
+                                <div class="align-stick-ver"><span class="stick-ver">|</span></div>
+                            </div>
+                            <div class="characteristic">
+                                <span>Characteristics</span>
+                            </div>
+                        </div>
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                      <div class="accordion-body characteristics-text">
+                        <div class="margin-characteristics">
+                            <span class="weight">Weight:</span>
+                        </div>
+                        <span>1.95 kg</span>
+                        <div class="margin-characteristics">
+                            <span class="speed">Max.speed:</span>
+                        </div>
+                        <span>56km/h(S-mode)</span>
+                        <div class="margin-characteristics">
+                            <span class="battery">Battery:</span>
+                        </div>
+                        <span>5100mAh</span>
+                        <div class="margin-characteristics">
+                            <span class="resolution">Max. resolution:</span>
+                        </div>
+                        <span>12.1MP</span>
+                        <div class="margin-characteristics">
+                            <span class="flight-time">Max, flight time:</span>
+                        </div>
+                        <span>20 minutes</span>
+                      </div>
+                </div>
+            </div>
+        </div>`,
+    
+
+        `
+        <div class="margin-righ">
+            <img src="./img/our-drones/dji-mavik-air.png" alt="dji-mavik-zoom_Pic" class="drone-pic">
+            <span class="text-under-pic">
+                <span class="drone-name">DJI MAVIK AIR</span>
+                <span class="drone-price-rent">from $90/mo.</span>
+            </span>
+            <img src="./img/our-drones/green-line-deco.svg" alt="">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                        <div class="open-characteristics">
+                            <div class="align-stick-hor">
+                                <span class="stick-hor">—</span>
+                                <div class="align-stick-ver"><span class="stick-ver">|</span></div>
+                            </div>
+                            <div class="characteristic">
+                                <span>Characteristics</span>
+                            </div>
+                        </div>
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                      <div class="accordion-body characteristics-text">
+                        <div class="margin-characteristics">
+                            <span class="weight">Weight:</span>
+                        </div>
+                        <span>430 g</span>
+                        <div class="margin-characteristics">
+                            <span class="speed">Max.speed:</span>
+                        </div>
+                        <span>68.4km/h</span>
+                        <div class="margin-characteristics">
+                            <span class="battery">Battery:</span>
+                        </div>
+                        <span>2375mAh</span>
+                        <div class="margin-characteristics">
+                            <span class="resolution">Max. resolution:</span>
+                        </div>
+                        <span>12MP</span>
+                        <div class="margin-characteristics">
+                            <span class="flight-time">Max, flight time:</span>
+                        </div>
+                        <span>21 minutes</span>
+                      </div>
+                </div>
+            </div>
+        </div>`,
+];
+
+let currentSlide = 0;
+const placeHolderForSlides = document.querySelector('.carousel-slides');
+
+function renderSlides() {
+    placeHolderForSlides.innerHTML = slidesInfo[currentSlide];
+    if(window.innerWidth >= 767){
+        let secondSlide = currentSlide+1 >= slidesInfo.length ? 0 : currentSlide+1;
+        placeHolderForSlides.innerHTML += slidesInfo[secondSlide];
+    }
+}
+renderSlides();
+
+//---------------------Next & Prev BTNS---------------------
+const prevBtn = document.querySelector('.left-arrow');
+const nextBtn = document.querySelector('.right-arrow');
+
+function nextSlide() {
+    currentSlide = currentSlide++ >= slidesInfo.length-1 ? 0 : currentSlide++;
+    renderSlides();
+}
+
+function prevSlide() {
+    currentSlide = currentSlide-- <= 0 ? slidesInfo.length-1 : currentSlide--;
+    renderSlides();
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+window.addEventListener('resize', renderSlides);
+//--------------------------------------------------------------
+
+
 //-----------------Animation-characteristics---------------
 const characteristicsBtn = document.querySelector('.open-characteristics');
 const stickVer = document.querySelector('.stick-ver');
@@ -6,9 +241,3 @@ characteristicsBtn.addEventListener('click', () =>{
     stickVer.classList.toggle('opacity-stick');
 });
 //--------------------------END-----------------------------
-const slides = [
-    ``,
-    ``,
-    ``,
-    ``
-];
